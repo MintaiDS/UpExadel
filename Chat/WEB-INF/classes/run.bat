e15189be72b0118fbb05fcc9ff03b0571d573e10
Sipeyko_D <MintaiDS@ya.ru>
2015-05-10 13:03:27 +0200

    Made some cleaning in repository

--- Chat/WEB-INF/classes/run.bat
+++
@@ -1,5 +0,0 @@
-javac -classpath "%CATALINA_HOME%/lib/servlet-api.jar;%CATALINA_HOME%/lib/json-simple-1.1.1.jar;../lib/postgresql-9.4-1201.jdbc4.jar" UpdateServlet.java -Xlint
-
-javac -classpath "%CATALINA_HOME%/lib/servlet-api.jar;%CATALINA_HOME%/lib/json-simple-1.1.1.jar;../lib/postgresql-9.4-1201.jdbc4.jar" GetServlet.java -Xlint
-
-javac -classpath "%CATALINA_HOME%/lib/servlet-api.jar;%CATALINA_HOME%/lib/json-simple-1.1.1.jar;../lib/postgresql-9.4-1201.jdbc4.jar" FirstServlet.java -Xlint
